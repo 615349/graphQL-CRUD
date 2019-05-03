feeb772965d9af14f0bf7e2836957741bb7225b0
Ben Li <ben.li@qantas.com.au>
2019-05-03 15:18:51 +1000

    schema

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
+const cors = require('cors');
+const schema = require('./schema/schema');
+
+const app = express();
+app.use(cors());
 
 const config = {
   promiseLibrary: Promise,
   useNewUrlParser: true
 }
-mongoose.connect('mongodb://localhost/node-graphql', config)
+mongoose.connect('mongodb://localhost/test', config)
   .then(() => console.log('connection successful'))
   .catch(err => console.error(err));
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for requests on port 4000');
+});
+
--- server/models/Book.js
+++ server/models/Book.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const BookSchema = new Schema({
   name: String,
-  genre: String
+  genre: String,
+  authorId: String
 });
 
 module.exports = mongoose.model('Book', BookSchema);
