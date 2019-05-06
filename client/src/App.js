deed9806a8f1dcf493149f908dcb8ee36217e5b6
Ben Li <ben.li@qantas.com.au>
2019-05-06 10:37:39 +1000

    client init commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Edit and save to reload.
+    </div>
+  );
+}
+
+export default App;
